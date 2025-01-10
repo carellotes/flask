748dd7dc2bd317652de6a2266bc137715b3548a8
치호 <85450466+carellotes@users.noreply.github.com>
2025-01-10 18:54:05 +0900

    Add files via upload

--- new.py
+++ new.py
@@ -36,13 +36,13 @@ def home():
     # post_message(myToken1,"#chiho",messages)
     return render_template("/index.html")
 
-@app.route('/templates')
-def hsome():
-    global iss 
-    iss+=1
-    messages = "서버작동동 후 " + str(iss) + " 명이 Home Page 방문왔습니다."
-    # post_message(myToken1,"#chiho",messages)
-    return render_template("/index.html")
+# @app.route('/templates')
+# def hsome():
+#     global iss 
+#     iss+=1
+#     messages = "서버작동동 후 " + str(iss) + " 명이 Home Page 방문왔습니다."
+#     # post_message(myToken1,"#chiho",messages)
+#     return render_template("/index.html")
 
 
 @app.route("/templates", methods=["POST","GET"])
