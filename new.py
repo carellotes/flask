8b2f519c9581a11bf510e0a423a761ef35d03542
치호 <85450466+carellotes@users.noreply.github.com>
2025-01-10 18:09:46 +0900

    Add files via upload

--- new.py
+++ new.py
@@ -4,19 +4,19 @@ from flask import Flask, redirect, render_template, url_for, request
 iss=0
 
 # import yfinance as yf
-import requests
-myToken1 = 'xoxb-8253119763543-8261667911078-dvXQn0n8Y7e6sRS7HHzDOn5J'
-def post_message(token, channel, text):
-        response = requests.post("https://slack.com/api/chat.postMessage",
-        headers={"Authorization": "Bearer "+token},
-        data={"channel": channel,"text": text}
-    )
-    # print(response)
+# import requests
+# myToken1 = 'xoxb-8253119763543-8261667911078-dvXQn0n8Y7e6sRS7HHzDOn5J'
+# def post_message(token, channel, text):
+#         response = requests.post("https://slack.com/api/chat.postMessage",
+#         headers={"Authorization": "Bearer "+token},
+#         data={"channel": channel,"text": text}
+#     )
+#     # print(response)
     
     
 
 messages = "서버가 초기화 되었습니다"
-post_message(myToken1,"#chiho",messages)
+# post_message(myToken1,"#chiho",messages)
 
 
 app = Flask(__name__)
@@ -30,24 +30,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
     global iss 
     iss+=1
-    
     messages = "서버작동동 후 " + str(iss) + " 명이 Home Page 방문왔습니다."
-    post_message(myToken1,"#chiho",messages)
-    
-    return render_template("index.html")
+    # post_message(myToken1,"#chiho",messages)
+    return render_template("/templates/index.html")
 
 @app.route('/templates')
 def hsome():
-    
     global iss 
     iss+=1
-    
     messages = "서버작동동 후 " + str(iss) + " 명이 Home Page 방문왔습니다."
-    post_message(myToken1,"#chiho",messages)
-    
+    # post_message(myToken1,"#chiho",messages)
     return render_template("index.html")
 
 
@@ -55,23 +49,23 @@ def hsome():
 def login():
      if request.method == "POST":
             user=request.form['name']
-            post_message(myToken1,"#chiho",user)
+            # post_message(myToken1,"#chiho",user)
             phone=request.form['phone']
-            post_message(myToken1,"#chiho",phone)
+            # post_message(myToken1,"#chiho",phone)
             email=request.form['email']
-            post_message(myToken1,"#chiho",email)
+            # post_message(myToken1,"#chiho",email)
             contend=request.form['contend']
-            post_message(myToken1,"#chiho",contend)
-            return render_template("goodbye.html", content=[user,phone,email,contend])
+            # post_message(myToken1,"#chiho",contend)
+            return render_template("/goodbye.html", content=[user,phone,email,contend])
      
      else:
-        return render_template("index.html")
+        return render_template("/templates/index.html")
 
 
 @app.route("/<usr>")
 def user(usr):
      messages=usr
-     post_message(myToken1,"#chiho",messages)
+    #  post_message(myToken1,"#chiho",messages)
      return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
